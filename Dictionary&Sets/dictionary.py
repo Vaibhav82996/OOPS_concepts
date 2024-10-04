830515976a259eab26299a7817877b6bb611168a
12344addff <143561368+12344addff@users.noreply.github.com>
2024-10-03 23:52:32 -0400

    Dictionary_python

---
+++ Dictionary&Sets/dictionary.py
@@ -0,0 +1,12 @@
+#Dictionaries are used to store data values in key.value pairs they are unorderd,mutable(changeble)
+#  & don't allow duplicate keys.
+info = {
+    "key" : "value",
+    "name" : "data science",
+    "age" : 35,
+    "is adult" : True,
+    "marks" : 33.22,
+    "subjects" : ["python", "c", "java"],
+    "topics" : ("dict","set")
+}
+print(type(info))
