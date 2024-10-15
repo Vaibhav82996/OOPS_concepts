61b3a8783c50f178b043c85c22ad4a9cb9fc4c90
12344addff <143561368+12344addff@users.noreply.github.com>
2024-10-14 23:50:40 -0400

    Function_python

---
+++ function&recursion/fun2.py
@@ -0,0 +1,15 @@
+#another exaample.
+def cal_sum(x,y):           # here x and y are parameters
+    sum = x+y
+    print(sum)
+    return sum
+cal_sum(22,44)          #here 22 and 44 is arguments wiith fun call before.
+
+#calculate the avg of 3 numbers.
+
+def avg_num(a,b,c):
+    sum = a+b+c
+    average = sum / 3
+    print(average)
+    return average
+avg_num(34,55,21)
---
+++ function&recursion/function.py
@@ -0,0 +1,20 @@
+#Block of code that perform specific task and used to avoide repetation tasks by using functions. using def keyword.
+def calc_sum(a,b):
+    sum = a+b
+    print(sum)
+    return sum
+calc_sum(10,5)
+
+# a = 12
+# b= 22
+# sum = a+b
+# print(sum)
+
+calc_sum(12,22)   # to avoide upper four lines
+
+# a = 22
+# b = 44
+# sum = a+b
+# print(sum)
+
+calc_sum(22,44)   # to avoide upper four lines
